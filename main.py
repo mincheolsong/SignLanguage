f6fd2ce24618d5928fed3fc1f231f4a30b8b947a
EeeUnS <euns312510@gmail.com>
2022-05-22 18:21:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,69 +6,65 @@ import keyboard
 import time
 import numpy as np
 import pandas as pd
-from PIL import ImageFont, ImageDraw, Image
 from tensorflow.keras.models import load_model
+from PIL import ImageFont, ImageDraw, Image, ImageTk
+from tkinter import Tk, Label, Button
 
-gesture = {
-    0:'ㄱ',1:'ㄴ',2:'ㄷ',3:'ㄹ',4:'ㅁ',5:'ㅂ',6:'ㅅ',7:'ㅇ',
-    8:'ㅈ',9:'ㅊ',10:'ㅋ',11:'ㅌ',12:'ㅍ',13:'ㅎ',14:'next',
-    15:'prev',16:'q',17:'r',18:'s',19:'t',20:'u',21:'v',
-    22:'w',23:'x',24:'y',25:'done',26:'spacing',27:'clear'
-}
-actions = ['prev', 'next'] # ★
-seq_length = 30 # ★
-next_cnt = 0
-previous_cnt = 0
-model = load_model('models/model.h5') # ★
+
+class App:
+    def __init__(self, windowtk):
+        self.width, self.height = 1280,720
+        self.delay = 33
+
+        self.gui = windowtk
+        self.gui.geometry("1280x800")
+
+
         
-seq = [] # ★
-action_seq = [] # ★
-last_action = None
+        self.textLabel = []
+        self.text = []
+        for i in range(0, 3) :  
+            self.text.append(str(i) + "가나다")
+            self.textLabel.append(Label())
+            self.textLabel[i].pack(side="bottom", anchor="s")
+            self.textLabel[i].configure(text=self.text[i], font=('Arial', 25))
+        print(self.text)
+        self.imageLabel = Label(self.gui, text="GUI")
+        self.imageLabel.pack()
+        
+        self.button = Button(
+            self.gui,
+            text='Clear',
+            padx=100, 
+            pady=80,
+            command=self.clear
+        )
+        self.button.pack(side="bottom", anchor="e")
 
-mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(
-    max_num_hands=1,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5,
-)
+    def draw(self, image):
+        imgtk = ImageTk.PhotoImage(image = Image.fromarray(image)) # ImageTk 객체로 변환
+        # OpenCV 동영상
+        self.imageLabel.imgtk = imgtk
+        self.imageLabel.configure(image=imgtk)
+ 
 
-f = open('test.txt','w')
-file = np.genfromtxt('dataSet.txt',delimiter=',')
-dic_file = pd.read_csv('dictionary.csv') #사전 파일
+    def clear(self):
+        self.text[0] = ""
+        self.text[1] = ""
+        self.text[2] = ""
+        self.textLabel[0].configure(text=self.text[0])
+        self.textLabel[1].configure(text=self.text[0])
+        self.textLabel[2].configure(text=self.text[0])
+    
+    def drawText(self, index ,text):
+        self.text[index] = self.text[index] + str(text)
+        self.textLabel[index].configure(text=self.text[index], font=('Arial', 25))
 
+##########################################################################################################
 
-angleFile = file[:,:-1]
-labelFile = file[:,-1]
-angle = angleFile.astype(np.float32)
-label = labelFile.astype(np.float32)
-
-knn = cv2.ml.KNearest_create()
-knn.train(angle, cv2.ml.ROW_SAMPLE, label)
-
-cap = cv2.VideoCapture(0)
-
-startTime = time.time()
-
-prev_index = 0
-sentence = ''
-recognizeDelay = 2
-
-b,g,r,a = 0,0,0,0
-fontpath = "C:\Windows\Fonts\gulim.ttc"
-font = ImageFont.truetype(fontpath, 50)
-
-complete = 0 #글씨 입력을 완료 했는지 확인하는 변수(done 동작을 하면 1로 바꿈)
-selected_words = []
-word=''
-calced_dist=0
-def calc_dist(x1,y1,x2,y2):
-    return math.sqrt(math.pow(x1-x2,2)+math.pow(y1-y2,2))
-
-while True:
+def update(gui):
+    global sentence, calced_dist, selected_words, complete, font, fontpath, b,g,r,a, recognizeDelay, sentence, prev_index, startTime, cap, knn, label, angle,labelFile, angleFile, dic_file, file, f, hands, mp_hands, mp_drawing, gesture, next_cnt, previous_cnt, model, last_action
     ret, image = cap.read()
-    if not ret:
-        continue
     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB) # OpenCV : BGR 사용, MediaPipe : RGB 사용
     result = hands.process(image) # 전처리 및 모델 추론을 함께 실행한다.
     img_pil = Image.fromarray(image)
@@ -147,7 +143,7 @@ while True:
 
     if complete==2:
         if len(seq) < seq_length:
-            continue
+            ret
 
         input_data = np.expand_dims(np.array(seq[-seq_length:], dtype=np.float32), axis=0)
 
@@ -157,13 +153,13 @@ while True:
         conf = y_pred[i_pred]
 
         if conf < 0.9:
-            continue
+            ret
 
         action = actions[i_pred]
         action_seq.append(action)
 
         if len(action_seq) < 3:
-            continue
+            ret
 
         this_action = '?'
         # if(hand_landmarks.landmark[0].x < 0.2 or hand_landmarks.landmark[0].x > 0.8):
@@ -205,13 +201,82 @@ while True:
     #print(sentence)
     # cv2.putText(image,sentence,(20,440),cv2.FONT_HERSHEY_SIMPLEX,2,(255,255,255),3)
     image = np.array(img_pil)
-    image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
-
-    cv2.imshow('image', image)
 
     cv2.waitKey(1)
     if keyboard.is_pressed('b'):
-        break
+        ret
+    gui.draw(image)
+    gui.gui.after(33, update, gui)
+
+
+def calc_dist(x1,y1,x2,y2):
+    return math.sqrt(math.pow(x1-x2,2)+math.pow(y1-y2,2))
+
+################################################################################################################
+#main
+
+
+
+gesture = {
+    0:'ㄱ',1:'ㄴ',2:'ㄷ',3:'ㄹ',4:'ㅁ',5:'ㅂ',6:'ㅅ',7:'ㅇ',
+    8:'ㅈ',9:'ㅊ',10:'ㅋ',11:'ㅌ',12:'ㅍ',13:'ㅎ',14:'next',
+    15:'prev',16:'q',17:'r',18:'s',19:'t',20:'u',21:'v',
+    22:'w',23:'x',24:'y',25:'done',26:'spacing',27:'clear'
+}
+actions = ['prev', 'next'] # ★
+seq_length = 30 # ★
+next_cnt = 0
+previous_cnt = 0
+model = load_model('models/model.h5') # ★
+        
+seq = [] # ★
+action_seq = [] # ★
+last_action = None
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(
+    max_num_hands=1,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5,
+)
+
+f = open('test.txt','w')
+file = np.genfromtxt('dataSet.txt',delimiter=',')
+dic_file = pd.read_csv('dictionary.csv') #사전 파일
+
+
+angleFile = file[:,:-1]
+labelFile = file[:,-1]
+angle = angleFile.astype(np.float32)
+label = labelFile.astype(np.float32)
+
+knn = cv2.ml.KNearest_create()
+knn.train(angle, cv2.ml.ROW_SAMPLE, label)
+
+cap = cv2.VideoCapture(0)
+
+startTime = time.time()
+
+prev_index = 0
+sentence = ''
+recognizeDelay = 2
+
+b,g,r,a = 0,0,0,0
+fontpath = "C:\Windows\Fonts\gulim.ttc"
+font = ImageFont.truetype(fontpath, 50)
+
+complete = 0 #글씨 입력을 완료 했는지 확인하는 변수(done 동작을 하면 1로 바꿈)
+selected_words = []
+word=''
+calced_dist=0
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+gui = App(Tk())
+update(gui)
+gui.gui.mainloop()
+
 
 f.close()
 
